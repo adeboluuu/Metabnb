83bced301a2ca250259a29b140bbbe5158ef9f81
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-11 00:57:59 +0100

    Trying to fix react-router bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home.jsx'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
  
 
   return (
-    <div className="App">
+    <>
       <Routes>
       <Route path ='/' element={ <Home/> }/>
       </Routes>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import '../assets/styles.css'
+
+function Home() {
+  return (
+    <div>
+        <p>Ade</p>
+    </div>
+  )
+}
+
+export default Home
