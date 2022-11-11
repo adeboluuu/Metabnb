5acb31b2352206c39e435e80d2489c78cfd68d87
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-11 01:16:38 +0100

    Fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <>
-      <Routes>
-      <Route path ='/' element={ <Home/> }/>
-      </Routes>
+    <Routes>
+    <Route path ='/' element={ <Home/> }/>
+    </Routes>
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import Nav from '../components/Nav'
 import '../assets/styles.css'
 
 function Home() {
   return (
     <div>
-        <p>Ade</p>
+      <Nav/>
     </div>
   )
 }
