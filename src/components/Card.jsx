0816fa3dec7dad8429c68ae56f992ea6d48f2d0a
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-13 20:47:53 +0100

    Changed img link to include the github repo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
   return (
     <div id='Places' className='xl:mx-15 lg:mx-10 md:mx-7 mx-auto my-2 xl:w-[252px] lg:10/12 md:w-1/4 w-4/6 xl:h-[370px] lg:h-[383px] md:[270px]'>
     <div className=' p-2.5 lg:flex-col block my-2 border-[#D7D7D7] rounded-2xl border-[1px] '>
-        <img src={` https://github.com/Metabnb/src/assets/${props.item.img}`} className='lg:w-[260px] md:w-[280px]'/>
+        <img src={` https://github.com/Metabnb/src/assets/${props.item.img}?raw=true`} className='lg:w-[260px] md:w-[280px]'/>
         <div className='text-[11px] pb-2 pt-4'>
         <span >Desert king</span><span className=' font-normal float-right  text-[#434343]'>1MBT per night</span>
         </div>
