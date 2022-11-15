84bf289132cb1a6e676b3b809e4ab6f541950ec5
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-15 12:32:15 +0100

    Cards responsiveness

--- src/components/Place-cards.jsx
+++ src/components/Place-cards.jsx
@@ -3,7 +3,7 @@ import Stars from '../assets/stars.png'
 function Placecards(props) {
   return (
 
-            <div id='Places' className='xl:mx-15 lg:mx-10 md:mx-7 mx-16 my-2 xl:w-[252px] lg:10/12 md:w-1/4 w-4/6 xl:h-[370px] lg:h-[383px] md:[270px]'>
+            <div id='Places' className='xl:mx-15 lg:mx-10 md:mx-13 mx-16 my-2 xl:w-[252px] lg:10/12 md:w-2/6 w-4/6 xl:h-[370px] lg:h-[383px] md:[270px]'>
     <div className=' p-2.5 lg:flex-col block my-2 border-[#D7D7D7] border shadow-xl rounded-2xl  '>
         <img src={`https://github.com/adeboluuu/Metabnb/blob/main/src/assets/${props.item.img}?raw=true`} className='lg:w-[260px] md:w-[280px]'/>
         <div className='text-[11px] pb-2 pt-4'>
